1aee6f63094a4401f045f8aecfe5c23b9e842fac
Kiliansobi <kilian.sobischek@live.de>
2020-12-23 14:48:21 +0100

    Schieben des Balkendiagramm Codes von Source zu working

--- source.R
+++ source.R
@@ -184,46 +184,4 @@ daily.incedence <- function(Daten, end = Sys.Date(), intervall = 7, relative = T
   return(daily.data)
 }
 
-#Balkendiagramm Fälle pro Tag Vorpräsentation 
-#Anpassen der Daten 
-cases.per.day.belgium <- select(cases.per.day, -Bayern, -Schweden, -Tschechien)
-cases.per.day.bayern <- select(cases.per.day, -Belgien, -Schweden, -Tschechien) 
-cases.per.day.schweden <- select(cases.per.day, -Bayern, -Belgien, -Tschechien) 
-cases.per.day.tschechien <- select(cases.per.day, -Bayern, -Schweden, -Belgien) 
-
-#Belgien
-balken.belgien <- ggplot(cases.per.day.belgium, aes(Datum, Belgien))+
-  geom_col(fill = "steelblue")+
-  ggtitle("Belgien")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  ylab("Fälle pro Tag")+
-  geom_hline(yintercept=1000, linetype="dashed", color = "red")+
-  geom_hline(yintercept = 5000, linetype = "dashed", color = "red")                    
-
-#Bayern
-balken.bayern  <- ggplot(cases.per.day.bayern, aes(Datum, Bayern))+
-  geom_col(fill = "steelblue")+
-  ggtitle("Bayern")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  ylab("Fälle pro Tag")+
-  geom_hline(yintercept=1000, linetype="dashed", color = "red")+
-  geom_hline(yintercept = 5000, linetype = "dashed", color = "red")                  
-
-#Tschechien
-balken.tschechien <- ggplot(cases.per.day.tschechien, aes(Datum, Tschechien))+
-  geom_col(fill = "steelblue")+
-  ggtitle("Tschechien")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  ylab("Fälle pro Tag")+
-  geom_hline(yintercept=1000, linetype="dashed", color = "red")+
-  geom_hline(yintercept = 5000, linetype = "dashed", color = "red")        
-
-#Schweden
-balken.schweden <- ggplot(cases.per.day.schweden, aes(Datum, Schweden))+
-  geom_col(fill = "steelblue")+
-  ggtitle("Schweden")+
-  theme(plot.title = element_text(hjust = 0.5))+
-  ylab("Fälle pro Tag")+
-  geom_hline(yintercept=1000, linetype="dashed", color = "red")+
-  geom_hline(yintercept = 5000, linetype = "dashed", color = "red")
 
